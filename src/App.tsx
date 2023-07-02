bce138748877bb7046a1b845c0f7f3592a201935
Codeyg12 <codey.gallup@gmail.com>
2023-07-02 13:02:55 -0600

    adding required changes to both App.tsx and Graph.tsx along with notes explaining

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  // adding a showGraph with a boolean value to state for continous action
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // initalize the state to false so user clicks to begin graph
+      showGraph: false
     };
   }
 
@@ -29,18 +33,32 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x = 0;
+    const getDataStreamer = () => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({
+          data: serverResponds,
+          showGraph: true
+        })
+      })
+      x++
+      if (x > 1000) {
+        clearInterval(interval)
+      }
+
+    }
+    const interval = setInterval(getDataStreamer, 100);
   }
 
   /**
